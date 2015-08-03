b84a96ef2a11499ed89f7272c9fc38e256102f21
nomss <nomana123@aim.com>
2015-08-03 13:14:56 -0400

    a

--- NomssWebsite/js/controller.js
+++ NomssWebsite/js/controller.js
@@ -1,5 +1,12 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        $("p").hide();
-    });
+$(document).ready(function() {
+	$("button").click(function() {
+		$("p").hide();
+	});
+
+	$.ajax({
+		url : "https://restcountries.eu/rest/v1/name/aruba?fullText=true"
+	}).then(function(data) {
+		$('.greeting-id').append(name);
+	});
+
 });
